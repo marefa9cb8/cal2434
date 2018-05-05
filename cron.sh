a8b20c6105c9215c05a4ea9470b085ab54ef60aa
balar R <balar@kmc.gr.jp>
2018-05-06 03:32:19 +0900

    cron

--- cron.sh
+++ cron.sh
@@ -14,3 +14,7 @@ cd `dirname $0`
 bundle exec ruby parse_wiki.rb
 bundle exec ruby parse_wiki_history.rb
 bundle exec ruby make_ics.rb > cal2434.ics
+
+git add wiki.yaml wiki_history.yaml cal2434.ics
+git -c "user.name=$CAL2324_COMMIT_USER" -c "user.email=$CAL2324_COMMIT_USER_EMAIL" commit -m "`date`"
+git push
